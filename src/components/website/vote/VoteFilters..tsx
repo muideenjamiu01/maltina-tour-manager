4e4578baee1d68b9fc6f07f60110b37af16bfaeb
Michelin <oluwabusiola@gmail.com>
2026-01-16 10:42:45 +0100

    Repush Voting-ui

--- src/components/website/vote/VoteFilters..tsx
+++ src/components/website/vote/VoteFilters..tsx
@@ -43,31 +43,7 @@ export default function VoteFilters({ filters, setFilters }: VoteFiltersProps) {
           </div>
         </div>
 
-        {/* Sort by */}
-        <div>
-          <span className="block text-sm font-medium mb-2">Sort by</span>
-
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 sm:max-w-[50%]">
-            <select
-              className="p-3 rounded-md border bg-gray-100 w-full"
-              value={filters.sort}
-              onChange={e => setFilters({ ...filters, sort: e.target.value })}
-            >
-              <option value="popular">Most Popular</option>
-              <option value="newest">Newest</option>
-            </select>
-
-            <select
-              className="p-3 rounded-md border bg-gray-100 w-full"
-              value={filters.time}
-              onChange={e => setFilters({ ...filters, time: e.target.value })}
-            >
-              <option value="all">All Time</option>
-              <option value="week">This Week</option>
-              <option value="today">Today</option>
-            </select>
-          </div>
-        </div>
+       
 
       </div>
     </section>
